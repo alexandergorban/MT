da2d8f5325b956b3625900f83a47d25b8ada99b7
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-11-16 17:01:44 +0200

    Seeding Default Data in EF Code First with the JSON Files.

--- WebApp/Models/MultiTenantContext.cs
+++ WebApp/Models/MultiTenantContext.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace WebApp.Models
 {
@@ -54,6 +58,94 @@ namespace WebApp.Models
                 };
             tenants.ForEach(a => context.Tenants.Add(a));
             context.SaveChanges();
+
+            CreateSpeakers(context);
+            CreateSessions(context);
+
+            //var list = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+
+            //var sessionJsonAll = GetEmbeddedResourceAsString("WebApp.session.json");
+        }
+
+        private void CreateSpeakers(MultiTenantContext context)
+        {
+            var speakerJsonAll = GetEmbeddedResourceAsString("WebApp.speaker.json");
+
+            JArray jsonValSpeakers = JArray.Parse(speakerJsonAll) as JArray;
+            dynamic speakersData = jsonValSpeakers;
+            foreach (dynamic speaker in speakersData)
+            {
+                context.Speakers.Add(new Speaker
+                {
+                    PictureId = speaker.id,
+                    FirstName = speaker.firstName,
+                    LastName = speaker.lastName,
+                    AllowHtml = speaker.allowHtml,
+                    Bio = speaker.bio,
+                    WebSite = speaker.webSite
+                });
+            }
+            context.SaveChanges();
+        }
+
+        private void CreateSessions(MultiTenantContext context)
+        {
+            var sessionJsonAll = GetEmbeddedResourceAsString("WebApp.session.json");
+            var tenants = context.Tenants.ToList();
+            JArray jsonValSessions = JArray.Parse(sessionJsonAll) as JArray;
+            dynamic sessionData = jsonValSessions;
+
+            var sessionTenantDict = new Dictionary<int, string>();
+
+            foreach (dynamic session in sessionData)
+            {
+                sessionTenantDict.Add((int)session.id, (string)session.tenantName);
+
+                var sessionForAdd = new Session
+                {
+                    SessionId = session.id,
+                    Description = session.description,
+                    DescriptionShort = session.descriotionShort,
+                    Title = session.title
+                };
+
+                var speakerPictureIds = new List<int>();
+                foreach (dynamic speaker in session.speakers)
+                {
+                    dynamic pictureId = speaker.id;
+                    speakerPictureIds.Add((int)pictureId);
+                }
+
+                sessionForAdd.Speakers = new List<Speaker>();
+                foreach (var speakerPictureId in speakerPictureIds)
+                {
+                    var speakerForAdd = context.Speakers.FirstOrDefault(a => a.PictureId == speakerPictureId);
+                    sessionForAdd.Speakers.Add(speakerForAdd);
+                }
+                context.Sessions.Add(sessionForAdd);
+            }
+
+            context.SaveChanges();
+            foreach (var session in context.Sessions)
+            {
+                var tenant = tenants.FirstOrDefault(a => a.Name == sessionTenantDict[session.SessionId]);
+                session.Tenant = tenant;
+            }
+            context.SaveChanges();
+        }
+
+        private string GetEmbeddedResourceAsString(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            string result;
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                result = reader.ReadToEnd();
+            }
+
+            return result;
         }
     }
 }
