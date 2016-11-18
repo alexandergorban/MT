945658048434c0bd3e83731a842c3d311c45f567
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-11-18 12:50:05 +0200

    Enhancing Our Controller to Support Async Await and Data Cache. Delete and Create Db.

--- WebApp/Controllers/SpeakerController.cs
+++ WebApp/Controllers/SpeakerController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 using WebApp.Models;
 
@@ -7,18 +9,49 @@ namespace WebApp.Controllers
 {
     public class SpeakerController : MultiTenantMvcController
     {
-        public ActionResult Index()
+        private MultiTenantContext context = new MultiTenantContext();
+
+        public async Task<ActionResult> Index()
         {
+            //var speakersAll = await context.Speakers.ToListAsync();
+
+            Task<List<Speaker>> speakersAll = new TCache<Task<List<Speaker>>>().
+                Get("s-cache", 20,
+                    () =>
+                    {
+                        var speakersAll1 = context.Speakers.ToListAsync();
+                        return speakersAll1;
+                    });
+
             var speakers = new List<Speaker>();
-            using (var context = new MultiTenantContext())
+            foreach (var speaker in await speakersAll)
             {
-                speakers =
-                    (from speaker in context.Speakers
-                     let sessionInTenant = speaker.Sessions.
-                     Any(a => a.Tenant.Name == Tenant.Name)
-                     where sessionInTenant
-                     select speaker).ToList();
+                bool speakersInTenant = speaker.Sessions.Any(a => a.Tenant.Name == Tenant.Name);
+                if (speakersInTenant)
+                {
+                    speakers.Add(new Speaker
+                    {
+                        FirstName = speaker.FirstName,
+                        LastName = speaker.LastName,
+                        Id = speaker.Id,
+                        PictureId = speaker.Id,
+                        Bio = speaker.Bio,
+                        AllowHtml = speaker.AllowHtml,
+                        WebSite = speaker.WebSite,
+                        Sessions = speaker.Sessions.
+                            Where(a => a.Tenant.Name == Tenant.Name).
+                            OrderBy(a => a.Title).ToList()
+                    });
+                }
             }
+
+            speakers =
+                (from speaker in context.Speakers
+                 let sessionInTenant = speaker.Sessions.
+                 Any(a => a.Tenant.Name == Tenant.Name)
+                 where sessionInTenant
+                 select speaker).ToList();
+
             return View("Index", speakers);
         }
     }
