940e5565042d541b23a482b0f92d26ae3435deff
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-11-28 23:31:28 +0200

    Adding Speaker Details Sessions and Home to the SVCC Tenant.

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -3,28 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : MultiTenantMvcController
     {
         public ActionResult Index()
         {
+            if (Tenant.Name == "CSSC")
+            {
+                using (var context = new MultiTenantContext())
+                {
+                    var sessions = context.Sessions.Where(a => a.Tenant.Name == Tenant.Name).
+                        OrderBy(a => a.Title.ToLower()).ToList();
+
+                    foreach (var session in sessions)
+                    {
+                        foreach (var speaker in session.Speakers)
+                        {
+                            speaker.ImageUrl = $"/Content/Images/Speakers/Speaker-{speaker.PictureId}-75.jpg";
+                        }
+                    }
+                    return View("Index", "_Layout", sessions);
+                }
+            }
+
             return View();
         }
 
-        public ActionResult About()
+        [MultiTenantControllerAllow("cssc")]
+        public ActionResult CodeOfConduct()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
