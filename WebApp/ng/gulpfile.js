d254dd4cfd789f4b9d6a97648415546aac1de8b8
Alexander Gorban <gorban.alexander.y@gmail.com>
2017-01-16 03:58:56 +0200

    Add watchify, and gulp task 'watch'.

--- WebApp/ng/gulpfile.js
+++ WebApp/ng/gulpfile.js
@@ -3,15 +3,23 @@ var gutil = require('gulp-util');
 var browserSync = require('browser-sync').create();
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var watchify = require('watchify');
 
 gulp.task('scripts', function () {
-    browserify('./svcc/main.js')
-        .bundle()
-        .on('error', function (e) {
-            gutil.log(e);
-        })
-        .pipe(source('app.js'))
-        .pipe(gulp.dest('./dist'));
+    return bundle(browserify('./svcc/main.js'));
+});
+
+gulp.task('watch', function () {
+    var watcher = watchify(browserify('./svcc/main.js', watchify.args));
+    bundle(watcher);
+    watcher.on('update', function () {
+        bundle(watcher);
+    });
+    watcher.on('update', gutil.log);
+
+    browserSync.init({
+        server: "./dist"
+    });
 });
 
 gulp.task('default', function () {
@@ -21,6 +29,16 @@ gulp.task('default', function () {
         }
     });
 });
+
+function bundle (bundler) {
+    return bundler.bundle()
+        .on('error', function (e) {
+            gutil.log(e);
+        })
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(browserSync.stream());
+}
 
 gulp.task('secondlog', function () {
     gutil.log('this is second log entry');
